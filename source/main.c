6ba9bcd6f9e2a07c8a43f80d0667f91ce1fa5faf
David Perez <dpere048@wch136-05.cs.ucr.edu>
2019-10-16 11:56:28 -0700

    fixed code

--- source/main.c
+++ source/main.c
@@ -14,29 +14,29 @@
 enum States{locked, unlocked, release, wait} state;
 unsigned char X;
 unsigned char Y;
-unsigned char #;
+unsigned char M;
 unsigned char Lock;
 unsigned char tmpB;
 
 void lock_Tick(){
 	X = PINA&0x01;  //PA0
 	Y = PINA&0x02; //PA1
-	# = PINA&0x03; //PA2 ..... PRESSING X AND THEN # UNLOCKS DOOR, SETS PB0 = 1
+	M  = PINA&0x03; //PA2 ..... PRESSING X AND THEN # UNLOCKS DOOR, SETS PB0 = 1
 	Lock = PINA&0x80; //LOCKS FROM INSIDE
 	switch(state){
 		case locked:
 			if(Lock){
 				state = locked;
 			}
-			else if(!X && !Y && # && !Lock){
-				wait;
+			else if(!X && !Y && M && !Lock){
+				state = wait;
 			}
 			else{
 				state = locked;
 			}
 			break;
 		case unlocked:
-			if(!X && !Y && !# && Lock){
+			if(!X && !Y && !M && Lock){
 				state = locked;
 			}
 			else{
@@ -44,10 +44,10 @@ void lock_Tick(){
 			}
 			break;
 		case release:
-			if(!X && !Y && !P && !Lock){
+			if(!X && !Y && !M && !Lock){
 				state = release;
 			}
-			else if(!X && Y && !# && !Lock){
+			else if(!X && Y && !M && !Lock){
 				state = unlocked;	
 			}
 			else{
@@ -55,10 +55,10 @@ void lock_Tick(){
 			}
 			break;
 		case wait:
-			if(!X && !Y && # && !Lock){
+			if(!X && !Y && M && !Lock){
 				state = wait;
 			}
-			else if(!X && !Y && !# && !Lock){
+			else if(!X && !Y && !M && !Lock){
 				state = release;
 			}
 			else{	
@@ -77,6 +77,8 @@ void lock_Tick(){
 			break;
 		case wait:
 			break;
+		}
+	}
 int main(void) {
     /* Insert DDR and PORT initializations */
 	DDRA = 0x00; PORTA = 0xFF;
